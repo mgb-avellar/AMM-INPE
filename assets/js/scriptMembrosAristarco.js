33f1cbeb2645a163b577410186462ee69dbb4f90
Marcio G B de Avellar <mgb.avellar@gmail.com>
2025-02-07 10:42:07 -0300

    Ajuste nas biografias

--- assets/js/scriptMembrosAristarco.js
+++ assets/js/scriptMembrosAristarco.js
@@ -1,9 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
     const container = document.getElementById("membrosAristarco-container");
+   
+    /*   
     const membros = [
         "marcioAlves.html", "camilaNovaes.html", "felipeFernandes.html", 
         "pedroBeaklini.html", "rodolfoValentim.html", "felipeNavarete.html", 
         "pauloCustodio.html", "jazielCoelho.html", "gardel.html"
+    ]; */
+
+    const membros = [
+        "pedroBeaklini.html", "rodolfoValentim.html", "felipeNavarete.html", 
+        "pauloCustodio.html", "gardel.html"
     ]; 
 
     membros.forEach(arquivo => {
